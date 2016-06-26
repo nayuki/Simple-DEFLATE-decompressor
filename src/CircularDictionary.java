02c2e98112114872318ada3c100141a03020979f
Project Nayuki <me@nayuki.io>
2016-06-26 21:05:44 +0000

    CircularDictionary: De-optimized but reduced code to not use bit masking for power-of-2 sizes, without changing behavior.

--- src/CircularDictionary.java
+++ src/CircularDictionary.java
@@ -13,8 +13,6 @@ final class CircularDictionary {
 	
 	private int index;
 	
-	private int mask;
-	
 	
 	
 	/*---- Constructor ----*/
@@ -28,11 +26,6 @@ final class CircularDictionary {
 			throw new IllegalArgumentException("Size must be positive");
 		data = new byte[size];
 		index = 0;
-		
-		if (size > 0 && (size & (size - 1)) == 0)  // Test if size is a power of 2
-			mask = size - 1;
-		else
-			mask = 0;
 	}
 	
 	
@@ -46,10 +39,7 @@ final class CircularDictionary {
 	 */
 	public void append(int b) {
 		data[index] = (byte)b;
-		if (mask != 0)
-			index = (index + 1) & mask;
-		else
-			index = (index + 1) % data.length;
+		index = (index + 1) % data.length;
 	}
 	
 	
@@ -72,22 +62,12 @@ final class CircularDictionary {
 		if (len < 0 || dist < 1 || dist > data.length)
 			throw new IllegalArgumentException();
 		
-		if (mask != 0) {
-			int readIndex = (index - dist + data.length) & mask;
-			for (int i = 0; i < len; i++) {
-				out.write(data[readIndex]);
-				data[index] = data[readIndex];
-				readIndex = (readIndex + 1) & mask;
-				index = (index + 1) & mask;
-			}
-		} else {
-			int readIndex = (index - dist + data.length) % data.length;
-			for (int i = 0; i < len; i++) {
-				out.write(data[readIndex]);
-				data[index] = data[readIndex];
-				readIndex = (readIndex + 1) % data.length;
-				index = (index + 1) % data.length;
-			}
+		int readIndex = (index - dist + data.length) % data.length;
+		for (int i = 0; i < len; i++) {
+			out.write(data[readIndex]);
+			data[index] = data[readIndex];
+			readIndex = (readIndex + 1) % data.length;
+			index = (index + 1) % data.length;
 		}
 	}
 	
