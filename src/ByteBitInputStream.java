832703ccec9f9aefdc32f22ae64020d7c1a22bb5
Project Nayuki <me@nayuki.io>
2018-11-13 16:52:54 +0000

    ByteBitInputStream: Deleted Javadoc comments that were cloned from the interface.

--- src/ByteBitInputStream.java
+++ src/ByteBitInputStream.java
@@ -51,10 +51,6 @@ public final class ByteBitInputStream implements BitInputStream {
 	
 	/*---- Methods ----*/
 	
-	/**
-	 * Returns the current bit position, which ascends from 0 to 7 as bits are read.
-	 * @return the current bit position, which is between 0 and 7
-	 */
 	public int getBitPosition() {
 		if (numBitsRemaining < 0 || numBitsRemaining > 7)
 			throw new AssertionError();
@@ -62,10 +58,6 @@ public final class ByteBitInputStream implements BitInputStream {
 	}
 	
 	
-	/**
-	 * Discards the remainder of the current byte (if any) and reads the next whole byte from the stream.
-	 * @return the next byte from the stream
-	 */
 	public int readByte() throws IOException {
 		currentByte = 0;
 		numBitsRemaining = 0;
@@ -73,12 +65,6 @@ public final class ByteBitInputStream implements BitInputStream {
 	}
 	
 	
-	/**
-	 * Reads a bit from this stream. Returns 0 or 1 if a bit is available, or -1 if
-	 * the end of stream is reached. The end of stream always occurs on a byte boundary.
-	 * @return the next bit of 0 or 1, or -1 for the end of stream
-	 * @throws IOException if an I/O exception occurred
-	 */
 	public int read() throws IOException {
 		if (currentByte == -1)
 			return -1;
@@ -95,13 +81,6 @@ public final class ByteBitInputStream implements BitInputStream {
 	}
 	
 	
-	/**
-	 * Reads a bit from this stream. Returns 0 or 1 if a bit is available, or throws an {@code EOFException}
-	 * if the end of stream is reached. The end of stream always occurs on a byte boundary.
-	 * @return the next bit of 0 or 1
-	 * @throws IOException if an I/O exception occurred
-	 * @throws EOFException if the end of stream is reached
-	 */
 	public int readNoEof() throws IOException {
 		int result = read();
 		if (result != -1)
@@ -111,10 +90,6 @@ public final class ByteBitInputStream implements BitInputStream {
 	}
 	
 	
-	/**
-	 * Closes this stream and the underlying input stream.
-	 * @throws IOException if an I/O exception occurred
-	 */
 	public void close() throws IOException {
 		input.close();
 		currentByte = -1;
